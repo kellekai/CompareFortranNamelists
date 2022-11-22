ff99907027c6a2d092169c2541d8f9511eb0bfda
Kai Keller <kai.keller@bsc.es>
2022-11-22 09:44:41 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#base_dir = "/home/kkeller/BSC/EarthScience/Projects/DestinationEarth_ECMWF/Lab/Nemo/DE_340_issue_152/"
+#v40_namelist_dir = "namelists_v40/"
+#v42_namelist_dir = "namelists_v42/"
+
+import f90nml
+
+class NemoNamelist:
+    def __init__(self, namelist_path):
+        self.namelist_path = namelist_path
+        self.namelist = f90nml.read(namelist_path)
+
+    def compare(self: 'NemoNamelist', reference: 'NemoNamelist'):
+        print("keys only in self")
+        for key in self.namelist.todict().keys():
+            if key not in reference.namelist.todict().keys():
+                print(f"\t: {key}")
+        print("keys only in reference")
+        for key in reference.namelist.todict().keys():
+            if key not in self.namelist.todict().keys():
+                print(f"\t: {key}")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
